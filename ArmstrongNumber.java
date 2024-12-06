d698f0a84777a53d55b076ba4d3130aaf84cc6b3
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-06 13:28:54 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int originalNumber = number;
+        int numberOfDigits = 0;
+        
+        while (number != 0) {
+            number /= 10;
+            numberOfDigits++;
+        }
+        
+        number = originalNumber;
+        
+        int sum = 0;
+        
+        while (number != 0) {
+            int digit = number % 10; 
+            sum += Math.pow(digit, numberOfDigits); 
+            number /= 10; 
+        }
+
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong number.");
+        }
+        scanner.close();
+    }
+}
